7cf53aeb2b2efeab5ac295819e843b0e2d51571b
Maximilian Adam <maxjadam@icloud.com>
2024-03-15 11:51:14 +0000

    partially working for loop

--- include/ast/blocks/ast_for.hpp
+++ include/ast/blocks/ast_for.hpp
@@ -29,38 +29,27 @@ public:
         initExpr->EmitRISC(stream, context);
         }
 
+        std::string label1 = context.makeName("L");
+        std::string label2 = context.makeName("L");
 
+        stream << label1 << ":" << std::endl;
 
+        if (condExpr != nullptr) {
+        condExpr->EmitRISC(stream, context);
+        stream << "beqz " << context.bindings[context.dst] << ", " << label2 << std::endl;
+        }
 
-        // if (expr ->getType() == "constant"){
-        //     while (expr -> getVal()){
-        //         statement -> EmitRISC(stream, context);
-        //     }
-        // }
-        // else if (expr ->getType() == "variable"){
-        //     std:: string label1 = context.makeName("L");
-        //     std::string label2 = context.makeName("L");
-        //     stream << "j " << label1 << std::endl;
-        //     stream << label2 << ":" << std::endl;
-        //     statement -> EmitRISC(stream, context);
-        //     stream << label1 << ":" << std::endl;
-        //     std:: string dst = context.AllocReg(expr->getId());
-        //     stream << "lw " << dst << "," << context.MemoryMapping[expr->getId()] << "(sp)" << std::endl;
-        //     stream << "ble " << dst << ",zero," << label2 << std::endl;
-        //     context.DeallocReg(expr->getId());
-        //     // theres a mistake here with the dealloc
-        // }
-        // else if (expr -> getType() == "operator"){
-        //     std:: string label1 = context.makeName("L");
-        //     std::string label2 = context.makeName("L");
-        //     stream << "j " << label1 << std::endl;
-        //     stream << label2 << ":" << std::endl;
-        //     statement -> EmitRISC(stream, context);
-        //     stream << label1 << ":" << std:: endl;
-        //     expr -> EmitRISC(stream, context);
-        //     stream << "ble " << context.bindings[context.dst] << ",zero," << label2 << std::endl;
-        //     context.DeallocReg(context.dst);
-        // }
+        if (statement != nullptr) {
+        statement->EmitRISC(stream, context);
+        }
+
+        if (incrExpr != nullptr) {
+        incrExpr->EmitRISC(stream, context);
+        }
+
+        stream << "j " << label1 << std::endl;
+
+        stream << label2 << ":" << std::endl;
 
     }
 

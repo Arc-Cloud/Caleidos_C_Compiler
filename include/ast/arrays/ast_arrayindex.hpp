fc9c47b7d4a5be30fe165e7bcdf70f8a8574bba7
Xylemeister <hanifrais17@gmail.com>
2024-03-17 05:48:20 +0000

    array done hvnt tested

--- include/ast/arrays/ast_arrayindex.hpp
+++ include/ast/arrays/ast_arrayindex.hpp
@@ -17,27 +17,73 @@ public:
         delete index;
     }
 
-    std:: string getId() const override{
-        return id -> getId();
+    std::string getId() const override
+    {
+        return id->getId();
     }
 
-    std:: string getType() const override{
-        return "array";
+    std::string getType() const override
+    {
+        if (index->getType() == "constant")
+        {
+            return "array1";
+        }
+        else
+        {
+            return "array";
+        }
     }
 
-    int getSize() const override{
-        return index -> getVal();
+    int getSize() const override
+    {
+        return index->getVal();
     }
     virtual void Print(std::ostream &stream) const override{};
 
     virtual void EmitRISC(std::ostream &stream, Context &context) const override
     {
-        if (index -> getType() == "constant"){
-            std:: string bind = context.makeName("A");
-            std:: string reg = context.AllocReg(bind);
-            std:: string mem = id ->getId() + std:: to_string(index->getVal());
-            stream << "lw " << reg << "," << context.MemoryMapping[mem] << "(sp)" << std::endl;
-            context.dst = bind;
+
+        if (context.ReadInstType() == "AssignArray")
+        {       
+                std:: string regs = context.dst;
+                std::string res = context.makeName("A");
+                std::string mem = id->getId() + "0";
+                index->EmitRISC(stream, context);
+                std::string index_ = context.bindings[context.dst];
+                std::string reg = context.dst;
+                stream << "slli " << index_ << "," << index_ << ",2" << std::endl; // since each word is 4
+                stream << "neg " << index_ << "," << index_ << std::endl;
+                stream << "add " << index_ << "," << index_ << ",sp" << std::endl;
+                stream << "sw " << context.bindings[regs] << "," << context.MemoryMapping[mem] << "(" << index_ << ")" << std::endl;
+                context.DeallocReg(regs);
+                context.DeallocReg(reg);
+                context.dst = res;
+        }
+        else
+        {
+            if (index->getType() == "constant")
+            {
+                std::string bind = context.makeName("A");
+                std::string reg = context.AllocReg(bind);
+                std::string mem = id->getId() + std::to_string(index->getVal());
+                stream << "lw " << reg << "," << context.MemoryMapping[mem] << "(sp)" << std::endl;
+                context.dst = bind;
+            }
+            else
+            {
+                std::string res = context.makeName("A");
+                std::string regs = context.AllocReg(res);
+                std::string mem = id->getId() + "0";
+                index->EmitRISC(stream, context);
+                std::string index_ = context.bindings[context.dst];
+                std::string reg = context.dst;
+                stream << "slli " << index_ << "," << index_ << ",2" << std::endl; // since each word is 4
+                stream << "neg " << index_ << "," << index_ << std::endl;
+                stream << "add " << index_ << "," << index_ << ",sp" << std::endl;
+                stream << "lw " << regs << "," << context.MemoryMapping[mem] << "(" << index_ << ")" << std::endl;
+                context.DeallocReg(reg);
+                context.dst = res;
+            }
         }
     };
 };
--- include/ast/operators/ast_assignment_operators.hpp
+++ include/ast/operators/ast_assignment_operators.hpp
@@ -3,38 +3,49 @@
 
 #include "../ast_node.hpp"
 
-class Assign : public Node{
-    private:
-    Node* identifier_;
-    Node* value_;
-    public:
-    Assign(Node* unary, Node* assign): identifier_(unary), value_(assign){};
-    virtual ~Assign(){
+class Assign : public Node
+{
+private:
+    Node *identifier_;
+    Node *value_;
+
+public:
+    Assign(Node *unary, Node *assign) : identifier_(unary), value_(assign){};
+    virtual ~Assign()
+    {
         delete identifier_;
         delete value_;
     }
-    virtual void Print(std::ostream &stream) const override{}; 
+    virtual void Print(std::ostream &stream) const override{};
 
-    std:: string getType() const override{
+    std::string getType() const override
+    {
         return "operator";
     }
 
     void EmitRISC(std::ostream &stream, Context &context) const override
     {
-        std:: string mem = identifier_->getId();
-       
-        value_-> EmitRISC(stream,context);
+        std::string mem = identifier_->getId();
 
-        if (identifier_ -> getType() == "array"){
-             mem = identifier_->getId() + std::to_string(identifier_->getSize());
+        value_->EmitRISC(stream, context);
+
+        if (identifier_->getType() == "array1")
+        {
+            mem = identifier_->getId() + std::to_string(identifier_->getSize());
         }
+       
+        if (identifier_->getType() == "array"){
+            context.WriteInstType("AssignArray");
+            identifier_->EmitRISC(stream,context);
+            context.WriteInstType(" ");
 
+        } else {
 
-
-        stream << "sw " << context.bindings[context.dst] << "," << context.MemoryMapping[mem] << "(sp)" << std:: endl;
+        stream << "sw " << context.bindings[context.dst] << "," << context.MemoryMapping[mem] << "(sp)" << std::endl;
         context.DeallocReg(context.dst);
+        }
     };
+    // this can be simplified
 };
-
 
 #endif
